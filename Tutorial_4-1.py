7ebd64bd94cbf64e28588ced9dc43c18600e074d
Juan Esteban Suárez Bravo <JuanesSB01@gmail.com>
2026-06-09 19:22:19 -0500

    Add files via upload

---
+++ Tutorial_4-1.py
@@ -0,0 +1,12 @@
+# Reto 1: Ingredientes de Pizza
+
+print("=== CREADOR DE PIZZA ===")
+
+while True:
+    ingrediente = input("Ingresa un ingrediente (o escribe 'salir'): ").lower()
+
+    if ingrediente == "salir":
+        print("Tu pizza está lista. ¡Buen provecho!")
+        break
+
+    print(f"Añadiré {ingrediente} a tu pizza.")
---
+++ Tutorial_4-2.py
@@ -0,0 +1,19 @@
+# Reto 2: Entradas de Cine
+
+print("=== SISTEMA DE ENTRADAS DE CINE ===")
+
+while True:
+    edad = input("Ingresa tu edad (o escribe 'salir'): ").lower()
+
+    if edad == "salir":
+        print("Gracias por visitarnos.")
+        break
+
+    edad = int(edad)
+
+    if edad < 3:
+        print("Tu entrada es GRATIS.")
+    elif edad <= 12:
+        print("Tu entrada cuesta $10.000.")
+    else:
+        print("Tu entrada cuesta $15.000.")
